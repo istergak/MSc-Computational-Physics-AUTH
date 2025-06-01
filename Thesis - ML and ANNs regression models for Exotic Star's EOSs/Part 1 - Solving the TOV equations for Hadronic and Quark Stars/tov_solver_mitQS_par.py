e8a8ecd5420c2ea6ce5ad507f5b07ab59199f2ed
Ioannis Stergakis <johnster789@gmail.com>
2025-06-01 22:03:09 +0300

    Add files via upload

--- Thesis - ML and ANNs regression models for Exotic Star's EOSs/Part 1 - Solving the TOV equations for Hadronic and Quark Stars/tov_solver_mitQS_par.py
+++ Thesis - ML and ANNs regression models for Exotic Star's EOSs/Part 1 - Solving the TOV equations for Hadronic and Quark Stars/tov_solver_mitQS_par.py
@@ -8,11 +8,11 @@
 # Implemented by: Ioannis Stergakis
 # AEM: 4439
 
-# Python Script: Py6a
-# Name: tov_solver_cflQS_par.py
+# Python Script: Py6b
+# Name: tov_solver_mitQS_par.py
 
 # Description: 
-# -> Parallel solving of the TOV equations for CFL EOS models of Quark Stars
+# -> Parallel solving of the TOV equations for MIT bag EOS models of Quark Stars
 # -> Storaging the solutions in .csv files
 
 # Abbrevations:
@@ -35,45 +35,28 @@ import time
 m_s = 95 # MeV, mass of Strange quark
 m_n = 939.565 # MeV, mass of Neutron
 hbarc = 197.327 # MeV*fm, hbar*c constant
-Beff_min = 57 # MeV*fm^-3, minimum value of the B_eff constant of the MIT bag model
-Delta_min = 50 # MeV, minimum value of the Δ constant of the MIT bag model
+B_ult_min = 57 # MeV*fm^-3, minimum value of the B constant of the MIT bag model
 
-# Defining the bound of the B_eff parameter for stable CFL phase of the quark matter as 
-# a function of the Δ parameter
-def B_eff_bound(Delta):
-    formula = -(m_s**2*m_n**2)/(12*(np.pi**2))+(Delta**2*m_n**2)/(3*(np.pi**2))+(m_n**4)/(108*(np.pi**2))
-    return formula/hbarc**3
-
-# Defining a function that finds all the CFL models with valid combinations of the Β_eff and Δ parameters
-# for stable CFL quark matter, depending on the maximum selected values of B_eff and Δ respectively,
-# as well as the step in scanning the B_eff axis and Δ axis
-
-def valid_cfl_models(Beff_max,Beff_step,Delta_max,Delta_step):
+# Defining a function that finds all the MIT bag models, provided
+# a maximum and a step for the bag parameter B. The minimum of B is fixed at 60 MeV*fm^-3
+def MIT_bag_models(B_max,B_step):
     models_names = []
-    valid_Beff = []
-    valid_Delta = []
-    valid_ms = []
+    B_values = []
 
     i = 1
-    for B_eff_val in range(60,Beff_max+Beff_step,Beff_step):
-        for Delta_val in range(Delta_min,Delta_max+Delta_step,Delta_step):
-            if B_eff_val < B_eff_bound(Delta_val):
-                models_names.append(f"CFL-{i}")
-                valid_ms.append(m_s)
-                valid_Beff.append(B_eff_val)
-                valid_Delta.append(Delta_val)
-                i = i + 1
+    for B_val in range(60,B_max+B_step,B_step):
+        models_names.append(f"MITbag-{i}")
+        B_values.append(B_val)
+        i=i+1
 
-    return [models_names,valid_Beff,valid_Delta,valid_ms]            
+    return [models_names,B_values]            
 
 # Define a function that returns and prints all the given combinations of B_eff and Δ parameters
 # in groups, depending on the result of the Beff_Delta_valid_combos() function
 # as well as the selected number of combinations per group
-def Beff_Delta_groups_maker(Beff_Delta_combos,num_per_group):
-    models_names = Beff_Delta_combos[0]
-    valid_Beff = Beff_Delta_combos[1]
-    valid_Delta = Beff_Delta_combos[2]
-    valid_ms = Beff_Delta_combos[3]
+def Beff_Delta_groups_maker(MITbag_combos,num_per_group):
+    models_names = MITbag_combos[0]
+    B_values = MITbag_combos[1]
 
     total_combos = len(models_names)
 
@@ -82,48 +65,36 @@ def Beff_Delta_groups_maker(Beff_Delta_combos,num_per_group):
     upper_limit = total_combos-mod
 
     models_names_groups = []
-    valid_Beff_groups = []
-    valid_Delta_groups = []
-    valid_ms_groups = []
+    B_values_groups = []
     
 
     for i in range(0,upper_limit,num_per_group):
         models_names_groups.append(models_names[i:i+num_per_group])
-        valid_Beff_groups.append(valid_Beff[i:i+num_per_group])
-        valid_Delta_groups.append(valid_Delta[i:i+num_per_group])
-        valid_ms_groups.append(valid_ms[i:i+num_per_group])
+        B_values_groups.append(B_values[i:i+num_per_group])
     if mod!=0:
         models_names_groups.append(models_names[upper_limit:total_combos])
-        valid_Beff_groups.append(valid_Beff[upper_limit:total_combos])
-        valid_Delta_groups.append(valid_Delta[upper_limit:total_combos])
-        valid_ms_groups.append(valid_ms[upper_limit:total_combos])
+        B_values_groups.append(B_values[upper_limit:total_combos])
     m = len(models_names_groups)
     for j in range(0,m):
         group_info = PrettyTable()
         group_info.add_column("Models",models_names_groups[j])
-        group_info.add_column("B_eff [MeV*fm^-3]",valid_Beff_groups[j])
-        group_info.add_column("Δ [MeV]",valid_Delta_groups[j])
-        group_info.add_column("m_s [MeV]",valid_ms_groups[j])
-        print(f"CFL Group {j+1}:")
+        group_info.add_column("B [MeV*fm^-3]",B_values_groups[j])
+        print(f"MIT-bag  Group {j+1}:")
         print(group_info,"\n")
     
-    return [models_names_groups,valid_Beff_groups,valid_Delta_groups,valid_ms_groups]
+    return [models_names_groups,B_values_groups]
 
-# Defining the generator for stable CFL quark matter EoSs of a QS 
-# as a function of the pressure P and the parameters B_eff and Δ
+# Defining the generator for MIT-bag quark matter EoSs of a QS 
+# as a function of the pressure P and the bag parameter B
 
-# Numerical definition
-def EOS_CFL_stable(p,B_eff,Delta):
-    a = -m_s**2/6 + 2*Delta**2/3
-    μ_sq = -3*a + np.sqrt(4/3*(np.pi**2)*(B_eff+p)*hbarc**3 + 9*a**2)
-    e = 3*p + 4*B_eff - 9*a*μ_sq/(hbarc**3*np.pi**2)
+# Numerical definition of EOS
+def EOS_MITbag(p,B):
+    e = 3*p + 4*B
     return e
 
-# Symbolic definition
-def EOS_CFL_stable_sym(p,B_eff,Delta):
-    a = -m_s**2/6 + 2*Delta**2/3
-    μ_sq = -3*a + smp.sqrt(4/3*(np.pi**2)*(B_eff+p)*hbarc**3 + 9*a**2)
-    e = 3*p + 4*B_eff-9*a * μ_sq/(hbarc**3*np.pi**2)
+# Numerical definition of the slope dE/dP of EOS
+def EOS_MITbag_slope(p):
+    e = 3
     return e
 
 
@@ -142,20 +113,15 @@ def tov_eq(r,y,EOS):
 
 # Defining a function that solves the system of TOV equations
 # for a given EOS
-def tov_sol(p_0,r_step,Beff_val,Delta_val,EOS_name):
+def tov_sol(p_0,r_step,B_val,EOS_name):
     # Defining the formula of the EOS function
     def EOS_formula(P):
-        return EOS_CFL_stable(P,Beff_val,Delta_val)
+        return EOS_MITbag(P,B_val)
     
-    # Defining the slope of the EOS function
-    p = smp.symbols("p")
-    def EOS_formula_slope(P_val):
-        return smp.diff(EOS_CFL_stable_sym(p,Beff_val,Delta_val),p).subs(p,P_val)
-
     # Calculating the mass density, energy density, EOS slope (dE/dP) and speed of sound c_s at NS center
     Pc = p_0 # pressure at QS center
     E_c = EOS_formula(Pc) # energy density at QS center
-    dE_dP = EOS_formula_slope(Pc) # EOS slope at QS center
+    dE_dP = EOS_MITbag_slope(Pc) # EOS slope at QS center
   
      
     # Pressure and mass initial values (initial conditions)       
@@ -220,7 +186,7 @@ def tov_sol(p_0,r_step,Beff_val,Delta_val,EOS_name):
         file.write(f"{P_out}, {Pc}, {E_c}, {dE_dP},{max(M_vals)}, {R_final}\n")
 
 # Defining the worker function for the parallel solving 
-def tov_sol_worker(task_id,EOS_name,Beff_value,Delta_value,ms_value,progress_queue):
+def tov_sol_worker(task_id,EOS_name,B_value,progress_queue):
 
     # Pressure ranges
     P_in_center_1: np.ndarray = np.arange(1,5,0.1) # lower pressure range
@@ -235,7 +201,7 @@ def tov_sol_worker(task_id,EOS_name,Beff_value,Delta_value,ms_value,progress_que
 
     filename = f'{EOS_name}_sol.csv'
     with open(filename,"w") as file:
-        file.write(f"P_out, P_c, E_c, dE_dP, M, R, B_eff={Beff_value:.1f},Delta={Delta_value:.1f}\n")
+        file.write(f"P_out, P_c, E_c, dE_dP, M, R, B={B_value:.1f}\n")
 
     for i in range(0,total_values):
         P_0 = P_in_center_total[i]
@@ -244,7 +210,7 @@ def tov_sol_worker(task_id,EOS_name,Beff_value,Delta_value,ms_value,progress_que
         progress_queue.put((task_id,EOS_name,i + 1, total_values,P_0))
         
         # Calling the tov_sol() function
-        tov_sol(P_0,R_step,Beff_value,Delta_value,EOS_name)
+        tov_sol(P_0,R_step,B_value,EOS_name)
 
 def print_progress(progress_queue, num_tasks):
     progress = {i: (0, 1, 2, 3) for i in range(num_tasks)}  # Dictionary to track (step, total_steps) for each task
@@ -275,13 +241,12 @@ def print_progress(progress_queue, num_tasks):
     print("\nElapsed time: %.1f\'%.2f\""%(cpu_time_total_mins,cpu_time_res))
     print('\n-------------------------------------------------------------------------------')
 
-def task_giver(models_selected,Beff_selected_group,Delta_selected_group,ms_selected_group,progress_queue):
+def task_giver(models_selected,B_selected_group,progress_queue):
     n = len(models_selected)
-        
     # Appending the tasks to be parallelized
     tasks = []
     for i in range(0,n):
-        tasks.append(pool.apply_async(tov_sol_worker, args=(i,models_selected[i],Beff_selected_group[i],Delta_selected_group[i],ms_selected_group[i],progress_queue))) 
+        tasks.append(pool.apply_async(tov_sol_worker, args=(i,models_selected[i],B_selected_group[i],progress_queue))) 
 
 
         
@@ -294,36 +259,24 @@ if __name__ == "__main__":
     print('>>Determination of the range for the B_eff and Δ parameters:')
     print('-------------------------------------------------------------------------------')
     
-    print("Minimum value of B_eff parameter in our study: 60.00 [MeV*fm^-3]")
+    print("Minimum value of bag parameter B in our study: 60.00 [MeV*fm^-3]")
     # Ask the user to give the maximum value for the Beff parameter
-    Beff_max_val = int(input("Give the maximum value of the B_eff parameter in [MeV*fm^-3]:  "))
-    while Beff_max_val<=57:
-        Beff_max_val = int(input("The value must be greater than 57 [MeV*fm^-3]. Try again:  "))
+    B_max_val = int(input("Give the maximum value of the bag parameter B in [MeV*fm^-3]:  "))
+    while B_max_val<=57:
+        B_max_val = int(input("The value must be greater than 57 [MeV*fm^-3]. Try again:  "))
 
     # Ask the user to give the step for the scan of the Beff area
-    Beff_step_val = int(input("Give the step for the scan of the Beff area in [MeV*fm^-3]:  "))
-    while Beff_step_val<=0:
-        Beff_step_val = int(input("The value must be positive. Try again:  "))
-    print('-------------------------------------------------------------------------------')
-    
-    print("Minimum value of Δ parameter in our study: %.2f [MeV]"%Delta_min)
-    # Ask the user to give the maximum value for the Δ parameter
-    Delta_max_val = int(input("Give the maximum value of the Δ parameter in [MeV]:  "))
-    while Delta_max_val<=50:
-        Delta_max_val = int(input("The value must be greater than 50 [MeV]. Try again:  "))
-
-    # Ask the user to give the step for the scan of the Δ area
-    Delta_step_val = int(input("Give the step for the scan of the Δ area in [MeV]:  "))
-    while Beff_step_val<=0:
-        Beff_step_val = int(input("The value must be positive. Try again:  "))         
+    B_step_val = int(input("Give the step for the scan of the B parameter area in [MeV*fm^-3]:  "))
+    while B_step_val<=0:
+        B_step_val = int(input("The value must be positive. Try again:  "))
     print('-------------------------------------------------------------------------------')
 
-    valid_combos_results = valid_cfl_models(Beff_max_val,Beff_step_val,Delta_max_val,Delta_step_val)
-    print("Total valid combinations:  %d"%len(valid_combos_results[0]))
+    MIT_bag_combos_results = MIT_bag_models(B_max_val,B_step_val)
+    print("Total valid combinations:  %d"%len(MIT_bag_combos_results[0]))
     print('-------------------------------------------------------------------------------')
 
     # Ask the user to give the number of valid Beff and Δ combinations per group
-    number_per_group = int(input("Give the number of valid Beff and Δ combinations per group:  "))
+    number_per_group = int(input("Give the number of valid MIT-bag models per group:  "))
     while number_per_group<=0:
         number_per_group = int(input("The number must be positive. Try again:  "))
     print('-------------------------------------------------------------------------------')
@@ -331,7 +284,7 @@ if __name__ == "__main__":
     print(">>Groups of models:")
     print('-------------------------------------------------------------------------------\n')
     # Getting and printing the groups of Γ combinations
-    models_names_groups,valid_Beff_groups,valid_Delta_groups,valid_ms_groups = Beff_Delta_groups_maker(valid_combos_results,number_per_group)
+    models_names_groups,B_groups = Beff_Delta_groups_maker(MIT_bag_combos_results,number_per_group)
     l = len(models_names_groups)
     
     print('-------------------------------------------------------------------------------')
@@ -340,9 +293,7 @@ if __name__ == "__main__":
         group_choosing = int(input("Group not found. Choose another group to be solved:  "))
     print('-------------------------------------------------------------------------------') 
     models_names_group_select = models_names_groups[group_choosing-1]
-    valid_Beff_group_select = valid_Beff_groups[group_choosing-1]
-    valid_Delta_group_select = valid_Delta_groups[group_choosing-1]
-    valid_ms_group_select = valid_ms_groups[group_choosing-1]
+    B_group_select = B_groups[group_choosing-1]
     num_tasks = len(models_names_group_select)
         
 
@@ -362,7 +313,7 @@ if __name__ == "__main__":
         # Supervising the solving process
         print("Supervising the solving process (current initial pressure values are being shown):")
         # Example: Each task has a different number of steps
-        tasks = task_giver(models_names_group_select,valid_Beff_group_select,valid_Delta_group_select,valid_ms_group_select,progress_queue)
+        tasks = task_giver(models_names_group_select,B_group_select,progress_queue)
 
         # Close the pool and wait for the workers to finish
         pool.close()
